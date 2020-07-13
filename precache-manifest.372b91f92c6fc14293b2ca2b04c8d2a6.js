8ceb6e3dc5e36e0cf6ddef7f37a4da7723114ac8
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-07-13 17:47:17 +0200

    Updates

---
+++ precache-manifest.372b91f92c6fc14293b2ca2b04c8d2a6.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8285b955c149616f5d3c39f9d2480cff",
+    "url": "/Recipes-World/index.html"
+  },
+  {
+    "revision": "0d7ec4ab07201de1fa2f",
+    "url": "/Recipes-World/static/css/2.b478278e.chunk.css"
+  },
+  {
+    "revision": "d1061ab5227028deda72",
+    "url": "/Recipes-World/static/css/main.ec84d79f.chunk.css"
+  },
+  {
+    "revision": "0d7ec4ab07201de1fa2f",
+    "url": "/Recipes-World/static/js/2.989a4483.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Recipes-World/static/js/2.989a4483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1061ab5227028deda72",
+    "url": "/Recipes-World/static/js/main.ebd796de.chunk.js"
+  },
+  {
+    "revision": "6a9ef2e930ce26d714f4",
+    "url": "/Recipes-World/static/js/runtime-main.9ee65084.js"
+  },
+  {
+    "revision": "a4a4c8bb0918020af0e8b829c0401a15",
+    "url": "/Recipes-World/static/media/avocado.a4a4c8bb.svg"
+  },
+  {
+    "revision": "0d0dae776a4075f9c990655090b9cee2",
+    "url": "/Recipes-World/static/media/banana.0d0dae77.svg"
+  },
+  {
+    "revision": "d5621f37ba5c9fc420d0f9c466d79493",
+    "url": "/Recipes-World/static/media/cherry.d5621f37.svg"
+  },
+  {
+    "revision": "05b5261937ee98d04a30054b9a89083d",
+    "url": "/Recipes-World/static/media/collage.05b52619.png"
+  },
+  {
+    "revision": "70b8cd702703d6a99f123ef9a5df0cd1",
+    "url": "/Recipes-World/static/media/decoration.70b8cd70.png"
+  },
+  {
+    "revision": "2912a435ee21e6d43c0fdafae9686c79",
+    "url": "/Recipes-World/static/media/fruit.2912a435.svg"
+  },
+  {
+    "revision": "11be6a407f71610d05cf35e895aab424",
+    "url": "/Recipes-World/static/media/grapes.11be6a40.svg"
+  },
+  {
+    "revision": "7367db138c4e788ca98b8c588e358e74",
+    "url": "/Recipes-World/static/media/line.7367db13.png"
+  },
+  {
+    "revision": "25c2003f5dac9889f61b8cba129f2c10",
+    "url": "/Recipes-World/static/media/mango.25c2003f.svg"
+  },
+  {
+    "revision": "1df1d4affa7c38216e583c26c298ffe1",
+    "url": "/Recipes-World/static/media/note.1df1d4af.png"
+  },
+  {
+    "revision": "e2abb56efb69077a749dd1b4603ddfde",
+    "url": "/Recipes-World/static/media/orange.e2abb56e.svg"
+  },
+  {
+    "revision": "8fa0ffebcb3d3492abef91fa92348301",
+    "url": "/Recipes-World/static/media/pear.8fa0ffeb.svg"
+  },
+  {
+    "revision": "4949b212ea34833ac4866101305fcfd2",
+    "url": "/Recipes-World/static/media/pineapple.4949b212.svg"
+  },
+  {
+    "revision": "0db18746629fad2fefa08ee526b4f75c",
+    "url": "/Recipes-World/static/media/raspberry.0db18746.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Recipes-World/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Recipes-World/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Recipes-World/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Recipes-World/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d3dd6d71ba007e84edf98ab0318179d5",
+    "url": "/Recipes-World/static/media/strawberry.d3dd6d71.svg"
+  },
+  {
+    "revision": "e7df78338a8478ecca68c5f17e5bb5d9",
+    "url": "/Recipes-World/static/media/watermelon.e7df7833.svg"
+  },
+  {
+    "revision": "9faebb4eecbd0ce3e0a03bd7b75b31e1",
+    "url": "/Recipes-World/static/media/welcome1.9faebb4e.jpg"
+  },
+  {
+    "revision": "b6e9fc6114a39b92b1afef4b4966e914",
+    "url": "/Recipes-World/static/media/welcome2.b6e9fc61.jpg"
+  },
+  {
+    "revision": "aae4108c02145f855da0fd80bf864dd6",
+    "url": "/Recipes-World/static/media/welcome_square1.aae4108c.jpg"
+  },
+  {
+    "revision": "8dc16e88182f01f7697c860721aa205e",
+    "url": "/Recipes-World/static/media/welcome_square2.8dc16e88.jpg"
+  },
+  {
+    "revision": "48167867fc25b7f7791b8afba162f3df",
+    "url": "/Recipes-World/static/media/welcome_square3.48167867.jpg"
+  },
+  {
+    "revision": "10887cc8cb6cabea5fd9dfa809cf9673",
+    "url": "/Recipes-World/static/media/welcome_square4.10887cc8.jpg"
+  },
+  {
+    "revision": "7fc55bbe716b7f3e9e1a79bdea1460f2",
+    "url": "/Recipes-World/static/media/welcome_square5.7fc55bbe.jpg"
+  },
+  {
+    "revision": "6c7595dcfabb9afa0005471e626db92b",
+    "url": "/Recipes-World/static/media/welcome_square6.6c7595dc.jpg"
+  }
+]);
